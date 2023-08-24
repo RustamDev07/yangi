bd7d4a36549131748df088c9f8da183849d1f4f8
RustamDev <mirzayevr04@gmail.com>
2023-08-24 18:29:59 +0500

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// alert("Salom Mening ismim Mirzayev Rustam");
+// let place = confirm("Sizning ismingiz Rustammi?");
+
+let currentplace = prompt("Ismingizni kiriting: ", "");
+console.log(currentplace);
